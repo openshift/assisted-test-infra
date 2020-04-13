b2d02e93e0507a58ef5eac36f560ee939be00c85
Igal Tsoiref <itsoiref@redhat.com>
2020-04-13 15:12:57 +0300

    Deleting worker dns

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -39,8 +39,6 @@ resource "libvirt_network" "net" {
       for_each = concat(
       data.libvirt_network_dns_host_template.masters.*.rendered,
       data.libvirt_network_dns_host_template.masters_int.*.rendered,
-      data.libvirt_network_dns_host_template.workers.*.rendered,
-      data.libvirt_network_dns_host_template.workers_int.*.rendered,
       )
       content {
         hostname = hosts.value.hostname
@@ -137,18 +135,5 @@ data "libvirt_network_dns_host_template" "masters" {
 data "libvirt_network_dns_host_template" "masters_int" {
   count    = var.master_count
   ip       = var.libvirt_master_ips[count.index]
-  hostname = "api-int.${var.cluster_domain}"
-}
-
-data "libvirt_network_dns_host_template" "workers" {
-  count    = 1
-  ip       = var.libvirt_worker_ips[count.index]
-  hostname = "api.${var.cluster_domain}"
-}
-
-
-data "libvirt_network_dns_host_template" "workers_int" {
-  count    = 1
-  ip       = var.libvirt_worker_ips[count.index]
   hostname = "api-int.${var.cluster_domain}"
 }
