5279add948b276a170578a0bcfb2737a508afed5
Igal Tsoiref <itsoiref@redhat.com>
2020-05-17 09:14:48 +0300

    Fixing issue with killing port forwarding

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -21,7 +21,7 @@ function run_in_background() {
 
 function kill_portforwarding_loop() {
   kill -9 $(ps aux | grep "port_forwarding_loop.sh $1 $2 $3" | grep -v grep | awk '{print $2}') || true
-  kill -9 $(ps aux | grep "kubectl --kubeconfig=${KUBECONFIG} port-forward | grep $3" | grep -v grep | awk '{print $2}') || true
+  kill -9 $(ps aux | grep "kubectl --kubeconfig=${KUBECONFIG} port-forward" | grep $3 | grep -v grep | awk '{print $2}') || true
 }
 
 function kill_all_port_forwardings() {
