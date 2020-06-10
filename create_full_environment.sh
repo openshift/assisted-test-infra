bad8f5106a7ba1f65e14ea32b548d6d352e52b57
Yuval Goldberg <yuvigoldi@hotmail.com>
2020-06-10 15:40:32 +0300

    MGMT-1111 Verify OS

--- create_full_environment.sh
+++ create_full_environment.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+
+function error () {
+    echo $@ 1>&2
+}
+
+# Check OS
+OS=$(awk -F= '/^ID=/ { print $2 }' /etc/os-release | tr -d '"')
+if [[ ! ${OS} =~ ^(centos)$ ]]; then
+  error "\"${OS}\" is an unsupported OS. We support only CentOS."
+  exit 1
+fi
+
+#Check CentOS version
+VER=$(awk -F= '/^VERSION_ID=/ { print $2 }' /etc/os-release | tr -d '"' | cut -f1 -d'.')
+VER_SUPPORTED=8
+
+if [[ ${VER} -ne ${VER_SUPPORTED} ]]; then
+  error "CentOS version ${VER_SUPPORTED} is required."
+  exit 1
+fi
+
 echo "Installing environment"
 sudo scripts/install_environment.sh
 echo "Done installing"
