8baba7e964ea565b0f7268b802e7daf09c49aa4f
Marc Sluiter <marc@slintes.net>
2020-07-09 20:31:24 +0200

    Fixed worker IP address when using non default network CIDR (#33)
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- discovery-infra/start_discovery.py
+++ discovery-infra/start_discovery.py
@@ -33,24 +33,28 @@ def fill_tfvars(image_path, storage_path, master_count, nodes_details):
 
     with open(consts.TFVARS_JSON_FILE) as _file:
         tfvars = json.load(_file)
-    network_subnet_starting_ip = str(
+    master_starting_ip = str(
         ipaddress.ip_address(
             ipaddress.IPv4Network(nodes_details["machine_cidr"]).network_address
         )
         + 10
     )
+    worker_starting_ip = str(
+        ipaddress.ip_address(
+            ipaddress.IPv4Network(nodes_details["machine_cidr"]).network_address
+        )
+        + 10 + tfvars["master_count"]
+    )
     tfvars["image_path"] = image_path
     tfvars["master_count"] = min(master_count, consts.NUMBER_OF_MASTERS)
     tfvars["libvirt_master_ips"] = _create_ip_address_list(
         min(master_count, consts.NUMBER_OF_MASTERS),
-        starting_ip_addr=network_subnet_starting_ip,
+        starting_ip_addr=master_starting_ip,
     )
     tfvars["api_vip"] = _get_vips_ips()[0]
     tfvars["libvirt_worker_ips"] = _create_ip_address_list(
         nodes_details["worker_count"],
-        starting_ip_addr=str(
-            ipaddress.ip_address(consts.STARTING_IP_ADDRESS) + tfvars["master_count"]
-        ),
+        starting_ip_addr=worker_starting_ip,
     )
     tfvars["libvirt_storage_pool_path"] = storage_path
     tfvars.update(nodes_details)
