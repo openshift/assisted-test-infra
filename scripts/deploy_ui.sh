c0e7a57ad71cbde8ffee0f2a8d2b1f8d3268e4e7
Vitaliy Emporopulo <vemporop@redhat.com>
2020-08-23 13:34:07 +0300

    MGMT-1723 Deploy assisted service and UI using manifests

--- scripts/deploy_ui.sh
+++ scripts/deploy_ui.sh
@@ -5,31 +5,17 @@ source scripts/utils.sh
 
 export NODE_IP=$(get_main_ip)
 export UI_PORT=${UI_PORT:-6008}
-export KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
-export CONTAINER_COMMAND=${CONTAINER_COMMAND:-podman}
-export UI_DEPLOY_FILE=build/ui_deploy.yaml
 export UI_SERVICE_NAME=ocp-metal-ui
 export NO_UI=${NO_UI:-n}
-export NAMESPACE=${NAMESPACE:-assisted-installer}
-if [ "${CONTAINER_COMMAND}" = "podman" ]; then
-    export PODMAN_FLAGS="--pull=always"
-else
-    export PODMAN_FLAGS=""
-fi
 
 if [ "${NO_UI}" != "n" ]; then
     exit 0
 fi
 
 mkdir -p build
-#In case deploy tag is empty use latest
-[[ -z "${DEPLOY_TAG}" ]] && export DEPLOY_TAG=latest
 
 print_log "Starting ui"
-
-${CONTAINER_COMMAND} pull quay.io/ocpmetal/ocp-metal-ui:latest
-${CONTAINER_COMMAND} run ${PODMAN_FLAGS} --rm quay.io/ocpmetal/ocp-metal-ui:latest /deploy/deploy_config.sh -u http://assisted-service.${NAMESPACE}.svc.cluster.local:8090 -i quay.io/ocpmetal/ocp-metal-ui:${DEPLOY_TAG} -n ${NAMESPACE} >${UI_DEPLOY_FILE}
-kubectl --kubeconfig=${KUBECONFIG} apply -f ${UI_DEPLOY_FILE}
+skipper run "make -C assisted-service/ deploy-ui" ${SKIPPER_PARAMS} DEPLOY_TAG=${DEPLOY_TAG} DEPLOY_MANIFEST_PATH=${DEPLOY_MANIFEST_PATH} DEPLOY_MANIFEST_TAG=${DEPLOY_MANIFEST_TAG} NAMESPACE=${NAMESPACE}
 
 print_log "Wait till ui api is ready"
 wait_for_url_and_run "$(minikube service ${UI_SERVICE_NAME} -n ${NAMESPACE} --url)" "echo \"waiting for ${UI_SERVICE_NAME}\""
