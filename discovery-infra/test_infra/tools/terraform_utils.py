3f092a3e489dc0977a2298dea2eb8603976a9472
Igal Tsoiref <itsoiref@redhat.com>
2020-11-03 22:01:06 +0200

    Terraform utils : add variables to file and then apply (#237)

--- discovery-infra/test_infra/tools/terraform_utils.py
+++ discovery-infra/test_infra/tools/terraform_utils.py
@@ -4,20 +4,33 @@ import logging
 
 class TerraformUtils:
 
+    VAR_FILE = "terraform.tfvars.json"
+
     def __init__(self, working_dir):
         logging.info("TF FOLDER %s ", working_dir)
         self.working_dir = working_dir
-        self.tf = Terraform(working_dir=working_dir, state="terraform.tfstate", var_file="terraform.tfvars.json")
+        self.var_file_path = os.path.join(working_dir, self.VAR_FILE)
+        self.tf = Terraform(working_dir=working_dir, state="terraform.tfstate", var_file=self.VAR_FILE)
         self.init_tf()
 
     def init_tf(self):
         self.tf.cmd("init -plugin-dir=/root/.terraform.d/plugins/", raise_on_error=True)
 
-    def apply(self):
-        self.tf.apply(no_color=IsFlagged, refresh=True, input=True, skip_plan=True, raise_on_error=True)
+    def apply(self, refresh=True):
+        return_value, output, err = self.tf.apply(no_color=IsFlagged, refresh=refresh, input=True, skip_plan=True)
+        if return_value != 0:
+            message = f'Terraform apply failed with return value {return_value}, output {output} , error {err}'
+            logging.error(message)
+            raise Exception(message)
 
-    def change_variables(self, variables):
-        self.tf.apply(no_color=IsFlagged, refresh=True, input=True, var=variables, skip_plan=True, raise_on_error=True)
+    def change_variables(self, variables, refresh=False):
+        with open(self.var_file_path, "r+") as _file:
+            tfvars = json.load(_file)
+            tfvars.update(variables)
+            _file.seek(0)
+            _file.truncate()
+            json.dump(tfvars, _file)
+        self.apply(refresh=refresh)
 
     def set_new_vip(self, api_vip):
         self.change_variables(variables={"api_vip": api_vip})
