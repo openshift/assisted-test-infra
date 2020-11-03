61db9e15d4605ec5ac38013f3db540e390e05b9f
Daniel Erez <derez@redhat.com>
2020-11-03 20:01:36 +0200

    e2e: test_select_roles (#233)
    Added a test for selecting hosts roles.

--- discovery-infra/tests/test_general.py
+++ discovery-infra/tests/test_general.py
@@ -1,5 +1,6 @@
 from tempfile import NamedTemporaryFile
 
+import consts
 import pytest
 from assisted_service_client.rest import ApiException
 
@@ -37,3 +38,16 @@ class TestGeneral(BaseTest):
         self.generate_and_download_image(cluster_id=cluster_id, api_client=api_client)
         node_controller.start_all_nodes()
         self.wait_until_hosts_are_discovered(cluster_id=cluster_id, api_client=api_client)
+        return cluster_id
+
+    def test_select_roles(self, api_client, cluster, node_controller):
+        cluster_id = self.test_discovery(api_client, cluster, node_controller)
+        self.set_host_roles(cluster_id=cluster_id, api_client=api_client)
+        hosts = api_client.get_cluster_hosts(cluster_id=cluster_id)
+        for node in hosts:
+            hostname = node["requested_hostname"]
+            role = node["role"]
+            if "master" in hostname:
+                assert role == consts.NodeRoles.MASTER
+            elif "worker" in hostname:
+                assert role == consts.NodeRoles.WORKER
