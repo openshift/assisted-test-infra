d86859135a8b5a462ec6ac7cb19daa2813d3b8b5
Ronnie Lazar <ronnie.lazar@redhat.com>
2020-11-08 17:43:15 +0200

    Added dry-run option

--- discovery-infra/add_triage_signature.py
+++ discovery-infra/add_triage_signature.py
@@ -309,6 +309,7 @@ def main(args):
 def add_signatures(jclient, url, issue_key, should_update=False):
     signatures = [ComponentsVersionSignature, HostsStatusSignature, HostsExtraDetailSignature, LibvirtRebootFlagSignature]
 
+
     for sig in signatures:
         s = sig(jclient)
         s.update_ticket(url, issue_key, should_update=should_update)
