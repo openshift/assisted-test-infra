091cf476cc2cc8b61fec4e3f4ea87e202163d62f
Daniel Erez <derez@redhat.com>
2020-11-19 01:54:07 +0200

    MGMT-2465: fix port on deploy_controller (#286)
    Service port on OCP cluster should be 7000.

--- scripts/deploy_controller.sh
+++ scripts/deploy_controller.sh
@@ -5,7 +5,7 @@ source scripts/utils.sh
 
 export SERVICE_URL=$(get_main_ip)
 export NAMESPACE=${NAMESPACE:-assisted-installer}
-export SERVICE_PORT=$(( 6000 + $NAMESPACE_INDEX ))
+export SERVICE_PORT=$(( 7000 + $NAMESPACE_INDEX ))
 export SERVICE_BASE_URL=${SERVICE_BASE_URL:-"http://${SERVICE_URL}:${SERVICE_PORT}"}
 
 mkdir -p build
