0459daa3447a4ec1e9971ae225cc731a91a27eaa
Yuval Goldberg <yuvigoldi@gmail.com>
2021-02-09 10:34:01 +0200

    Update default_ocp_versions only if override image provided

--- scripts/deploy_assisted_service.sh
+++ scripts/deploy_assisted_service.sh
@@ -17,8 +17,10 @@ export PUBLIC_CONTAINER_REGISTRIES=${PUBLIC_CONTAINER_REGISTRIES:-}
 
 mkdir -p build
 
-./assisted-service/tools/handle_ocp_versions.py --src ./assisted-service/default_ocp_versions.json \
-    --dest ./assisted-service/default_ocp_versions.json --ocp-override ${OPENSHIFT_INSTALL_RELEASE_IMAGE}
+if [ "${OPENSHIFT_INSTALL_RELEASE_IMAGE}" != "" ]; then
+    ./assisted-service/tools/handle_ocp_versions.py --src ./assisted-service/default_ocp_versions.json \
+        --dest ./assisted-service/default_ocp_versions.json --ocp-override ${OPENSHIFT_INSTALL_RELEASE_IMAGE}
+fi
 
 if [ "${DEPLOY_TARGET}" == "onprem" ]; then
     if [ -n "${INSTALLER_IMAGE:-}" ]; then
