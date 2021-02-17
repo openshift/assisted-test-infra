94475e1cb7a9f22a97039ce10ba4833aff7ac401
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2021-02-17 13:20:38 +0100

    fix(install_minikube.sh): usr/local/bin is not in default 'root' $PATH, only for users, but '/usr/local/sbin' is
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- scripts/install_minikube.sh
+++ scripts/install_minikube.sh
@@ -11,7 +11,7 @@ function install_minikube() {
         echo "Installing minikube..."
         curl --retry 3 -Lo minikube https://storage.googleapis.com/minikube/releases/v1.8.2/minikube-linux-amd64
         chmod +x minikube
-        ${SUDO} cp minikube /usr/bin/
+        ${SUDO} cp minikube /usr/local/sbin/
     else
         echo "minikube is already installed"
     fi
@@ -22,7 +22,7 @@ function install_kubectl() {
         echo "Installing kubectl..."
         curl --retry 3 -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl
         chmod +x kubectl
-        ${SUDO} mv kubectl /usr/bin/
+        ${SUDO} mv kubectl /usr/local/sbin/
     else
         echo "kubectl is already installed"
     fi
@@ -32,7 +32,7 @@ function install_oc() {
     if ! [ -x "$(command -v oc)" ]; then
         echo "Installing oc..."
         for i in {1..4}; do
-            curl --retry 3 -SL https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.6.0/openshift-client-linux-4.6.0.tar.gz | tar -xz -C /usr/local/bin && break
+            curl --retry 3 -SL https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.6.0/openshift-client-linux-4.6.0.tar.gz | tar -xz -C /usr/local/sbin && break
             echo "command failed. Retrying again in 5 seconds..."
             sleep 5
         done
