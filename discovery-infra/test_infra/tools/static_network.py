8708ad01c01895c46f5b81433633a30265ad396f
Yevgeny Shnaidman <yshnaidm@redhat.com>
2021-03-10 15:47:02 +0200

    MGMT-4472 - update test infra to use the new REST API for static network config

--- discovery-infra/test_infra/tools/static_network.py
+++ discovery-infra/test_infra/tools/static_network.py
@@ -6,6 +6,9 @@ from ipaddress import ip_network, ip_address
 
 from test_infra import consts, utils
 
+PRIMARY_LOGICAL_INTERFACE = "eth0"
+SECONDARY_LOGICAL_INTERFACE = "eth1"
+
 
 def generate_macs(count):
     return ["02:00:00:%02x:%02x:%02x" % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
@@ -51,15 +54,16 @@ def _prepare_host_static_network_data(primary_mac,
                            'dns-resolver': dns_resolver,
                            'routes': routes}
 
-    return yaml.dump(host_network_config)
+    mac_interface_map = [{'mac_address': primary_mac, 'logical_nic_name': PRIMARY_LOGICAL_INTERFACE}, {'mac_address': secondary_mac, 'logical_nic_name': SECONDARY_LOGICAL_INTERFACE}]
+    return {'network_yaml': yaml.dump(host_network_config), 'mac_interface_map': mac_interface_map}
 
 
 def _prepare_interfaces(primary_mac, secondary_mac, machine_cidrs, provisioning_cidrs, count):
-    primary_interface = _prepare_interface(primary_mac, machine_cidrs, count)
-    secondary_interface = _prepare_interface(secondary_mac, provisioning_cidrs, count)
+    primary_interface = _prepare_interface(primary_mac, PRIMARY_LOGICAL_INTERFACE, machine_cidrs, count)
+    secondary_interface = _prepare_interface(secondary_mac, SECONDARY_LOGICAL_INTERFACE, provisioning_cidrs, count)
     return [primary_interface, secondary_interface]
 
-def _prepare_interface(mac, cidrs, host_count):
+def _prepare_interface(mac, logical_interface, cidrs, host_count):
     ipv4_dict = {}
     ipv6_dict = {}
 
@@ -73,7 +77,7 @@ def _prepare_interface(mac, cidrs, host_count):
     if not ipv6_version and len(cidrs) > 1:
         _, ipv6_dict = _prepare_ip_dict(cidrs[1], host_count)
 
-    interface = {'name': mac.replace(':', ''),
+    interface = {'name': logical_interface,
                  'type': 'ethernet',
                  'state': 'up'}
     if ipv4_dict:
@@ -100,4 +104,4 @@ def _prepare_routes(primary_mac, machine_cidrs):
     network = ip_network(machine_cidrs[0])
     gw_ip = str(ip_address(network.network_address) + 1)
     default_route = '::/0' if network.version == 6 else '0.0.0.0/0'
-    return {'config': [{'destination': default_route , 'next-hop-address': gw_ip, 'next-hop-interface': primary_mac.replace(':', ''), 'table-id': 254}]}
+    return {'config': [{'destination': default_route , 'next-hop-address': gw_ip, 'next-hop-interface': PRIMARY_LOGICAL_INTERFACE, 'table-id': 254}]}
