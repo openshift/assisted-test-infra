9772aafc5ef78b5a38157cf57268d69ebb44bfec
Yuval Goldberg <yuvigoldi@gmail.com>
2021-04-20 13:08:43 +0300

    Revert "MGMT-5269 fix ENABLE_KUBE_API check on deploy service (#713)"
    This reverts commit 29c39f320d69992626c00e88312d352632aca183.

--- scripts/deploy_assisted_service.sh
+++ scripts/deploy_assisted_service.sh
@@ -15,17 +15,15 @@ export OCP_SERVICE_PORT=$(( 7000 + $NAMESPACE_INDEX ))
 export OPENSHIFT_INSTALL_RELEASE_IMAGE=${OPENSHIFT_INSTALL_RELEASE_IMAGE:-}
 export ENABLE_KUBE_API=${ENABLE_KUBE_API:-false}
 export ENABLE_KUBE_API_CMD="ENABLE_KUBE_API=${ENABLE_KUBE_API}"
-export OPENSHIFT_VERSIONS=${OPENSHIFT_VERSIONS:-}
-export OPENSHIFT_VERSIONS_CMD=""
+export OPENSHIFT_VERSIONS=${OPENSHIFT_VERSIONS:-"{}"}
 
-if [[ "${ENABLE_KUBE_API}" == "true" && -z "${OPENSHIFT_VERSIONS}" ]]; then
+if [[ ! -z ${ENABLE_KUBE_API:-} && "${OPENSHIFT_VERSIONS}" == "{}" ]]; then
     default_version=$(cat assisted-service/default_ocp_versions.json |
         jq -rc 'with_entries(select(.value.default == true))')
         # ToDo: include only 'rhcos_image' and 'rhcos_version' when custom
         #       OCP version is supported in assisted-service (MGMT-4554)
     json_template=\''%s'\'
     OPENSHIFT_VERSIONS=$(printf "$json_template" "$default_version")
-    OPENSHIFT_VERSIONS_CMD="OPENSHIFT_VERSIONS=${OPENSHIFT_VERSIONS}"
 fi
 
 mkdir -p build
@@ -75,7 +73,7 @@ elif [ "${DEPLOY_TARGET}" == "ocp" ]; then
 else
     print_log "Updating assisted_service params"
     skipper run discovery-infra/update_assisted_service_cm.py
-    (cd assisted-service/ && skipper --env-file ../skipper.env run "make deploy-all" ${SKIPPER_PARAMS} $ENABLE_KUBE_API_CMD $OPENSHIFT_VERSIONS_CMD DEPLOY_TAG=${DEPLOY_TAG} DEPLOY_MANIFEST_PATH=${DEPLOY_MANIFEST_PATH} DEPLOY_MANIFEST_TAG=${DEPLOY_MANIFEST_TAG} NAMESPACE=${NAMESPACE} AUTH_TYPE=${AUTH_TYPE})
+    (cd assisted-service/ && skipper --env-file ../skipper.env run "make deploy-all" ${SKIPPER_PARAMS} $ENABLE_KUBE_API_CMD DEPLOY_TAG=${DEPLOY_TAG} DEPLOY_MANIFEST_PATH=${DEPLOY_MANIFEST_PATH} DEPLOY_MANIFEST_TAG=${DEPLOY_MANIFEST_TAG} NAMESPACE=${NAMESPACE} AUTH_TYPE=${AUTH_TYPE} OPENSHIFT_VERSIONS=${OPENSHIFT_VERSIONS})
 
     add_firewalld_port $SERVICE_PORT
 
