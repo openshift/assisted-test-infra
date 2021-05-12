23a82a54e5c2a9cf8b4a741a4fba1f184f0b86a6
Yuval Goldberg <yuvigoldi@gmail.com>
2021-05-12 09:24:09 +0300

    MGMT-5274 Deploy assisted operator
    Add new 'operator' DEPLOY_TARGET to deploy_assisted_service.sh that
    calls deploy/operator/deploy.sh script from assisted-service repository.
    
    The current workflow to install an operator is to make a regular
    installation with AI, and afterwards use its KUBECONFIG to deploy the
    assisted-operator on top of it.
    
    A cluster installed with test-infra is provisoned with nat networking.
    It created iptables rules by design to ignore inbound connections except
    from the host.
    Hence, in order to make new VMs from a new network to reach the
    assisted operator that is installed on the cluster - we have to pass the
    communication through the host. It is made by overriding
    SERVICE_BASE_URL.
    
    Before deploying, we will start an nginx load balacer and configure it
    to listen to http and proxy pass the communication to the OCP route.
    The OCP route looks at the layer 7 Host field for destination - so we
    change that as well with "proxy_set_header Host" configuration.

--- scripts/deploy_assisted_service.sh
+++ scripts/deploy_assisted_service.sh
@@ -18,14 +18,13 @@ export ENABLE_KUBE_API_CMD="ENABLE_KUBE_API=${ENABLE_KUBE_API}"
 export OPENSHIFT_VERSIONS=${OPENSHIFT_VERSIONS:-}
 export OPENSHIFT_VERSIONS_CMD=""
 
-if [[ "${ENABLE_KUBE_API}" == "true" && -z "${OPENSHIFT_VERSIONS}" ]]; then
+if [[ "${ENABLE_KUBE_API}" == "true" || "${DEPLOY_TARGET}" == "operator" && -z "${OPENSHIFT_VERSIONS}" ]]; then
     # Supporting version 4.8 for kube-api
-    supported_version=$(cat assisted-service/default_ocp_versions.json |
+    OPENSHIFT_VERSIONS=$(cat assisted-service/default_ocp_versions.json |
        jq -rc 'with_entries(.key = "4.8") | with_entries(
            {key: .key, value: {rhcos_image: .value.rhcos_image, rhcos_version: .value.rhcos_version}})')
     json_template=\''%s'\'
-    OPENSHIFT_VERSIONS=$(printf "$json_template" "$supported_version")
-    OPENSHIFT_VERSIONS_CMD="OPENSHIFT_VERSIONS=${OPENSHIFT_VERSIONS}"
+    OPENSHIFT_VERSIONS_CMD="OPENSHIFT_VERSIONS=$(printf "${json_template}" "${OPENSHIFT_VERSIONS}")"
 fi
 
 mkdir -p build
@@ -72,6 +71,40 @@ elif [ "${DEPLOY_TARGET}" == "ocp" ]; then
 
     wait_for_url_and_run "$SERVICE_BASE_URL" "spawn_port_forwarding_command $SERVICE_NAME $OCP_SERVICE_PORT $NAMESPACE $NAMESPACE_INDEX $OCP_KUBECONFIG ocp $CLUSTER_VIP $SERVICE_NODEPORT"
     print_log "${SERVICE_NAME} can be reached at ${SERVICE_BASE_URL}"
+elif [ "${DEPLOY_TARGET}" == "operator" ]; then
+    # This nginx would listen to http on OCP_SERVICE_PORT and it would proxy_pass it to the actual route.
+    export SERVICE_BASE_URL=http://${SERVICE_URL}:${OCP_SERVICE_PORT}
+    add_firewalld_port ${OCP_SERVICE_PORT}
+
+    # TODO: Find a way to get the route dest dynamically.
+    # Currently it's not possible since it would be available only after the operator would be deployed
+    # The deploy.sh script would wait for the operator to succeed - so we need to have the LB before that.
+    # ROUTE=$(kubectl get routes -n assisted-installer --no-headers | awk '{print $2}')
+
+    ROUTE=assisted-service-assisted-installer.apps.test-infra-cluster-assisted-installer.redhat.com
+
+    tee << EOF ${HOME}/.test-infra/etc/nginx/conf.d/http_localhost.conf
+upstream upstream_${SERVICE_URL//./_} {
+    server ${ROUTE}:443;
+}
+
+server {
+    listen ${SERVICE_URL}:${OCP_SERVICE_PORT};
+
+    location / {
+        proxy_pass https://upstream_${SERVICE_URL//./_};
+        proxy_set_header Host ${ROUTE};
+    }
+}
+EOF
+
+    export DISKS="${LSO_DISKS:-}"
+    export ASSISTED_NAMESPACE=${NAMESPACE}
+    export SERVICE_IMAGE=${SERVICE}
+    export STORAGE_CLASS_NAME="localblock-sc"
+
+    INSTALL_LSO=false ./assisted-service/deploy/operator/deploy.sh
+    echo "Installation of Assisted Install operator passed successfully!"
 else
     print_log "Updating assisted_service params"
     skipper run discovery-infra/update_assisted_service_cm.py
