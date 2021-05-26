705b90f0cc3cb9482c860cd13cb93844dfda053d
Elior Erez <eerez@redhat.com>
2021-05-26 10:35:35 +0300

    MGMT-6534 - Fix PEP8 violations on tools

--- discovery-infra/test_infra/tools/concurrently.py
+++ discovery-infra/test_infra/tools/concurrently.py
@@ -1,8 +1,9 @@
 import logging
 from concurrent.futures import ThreadPoolExecutor
+from typing import Callable, Dict, List, Tuple, Union, Any
 
 
-def _safe_run(job, job_id, done_handler):
+def _safe_run(job, job_id: int, done_handler: Callable[[int], None]):
     call = None
     try:
         call, call_args = job[0], job[1:]
@@ -15,13 +16,17 @@ def _safe_run(job, job_id, done_handler):
             done_handler(job_id)
 
 
-def run_concurrently(jobs, done_handler=None, max_workers=5, timeout=2 ** 31):
+def run_concurrently(
+    jobs: Union[List, Dict, Tuple],
+    done_handler: Callable[[int], None] = None,
+    max_workers: int = 5,
+    timeout: float = 2 ** 31,
+) -> Dict[int, Any]:
     result = {}
     if isinstance(jobs, (list, tuple)):
         jobs = dict(enumerate(jobs))
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = [(job_id, executor.submit(_safe_run, *(job, job_id, done_handler)))
-                   for job_id, job in jobs.items()]
+        futures = [(job_id, executor.submit(_safe_run, *(job, job_id, done_handler))) for job_id, job in jobs.items()]
         for job_id, future in futures:
             result[job_id] = future.result(timeout=timeout)
 
