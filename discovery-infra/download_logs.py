0208299e83083e817d39ff87cdd21648693fd386
osher cohen <oscohen@localhost.localdomain>
2021-06-16 21:28:52 +0300

    NO-ISSUE: Add hosts to cluster metadata while downloading logs

--- discovery-infra/download_logs.py
+++ discovery-infra/download_logs.py
@@ -116,6 +116,11 @@ def is_update_needed(output_folder: str, update_on_events_update: bool, client: 
 
 def download_logs(client: InventoryClient, cluster: dict, dest: str, must_gather: bool,
                   update_by_events: bool = False, retry_interval: int = RETRY_INTERVAL, pull_secret=""):
+
+    # TODO remove when cluster_list has param to include download hosts
+    if "hosts" not in cluster:
+        cluster["hosts"] = client.get_cluster_hosts(cluster_id=cluster["id"])
+
     output_folder = get_logs_output_folder(dest, cluster)
     if not is_update_needed(output_folder, update_by_events, client, cluster):
         log.info(f"Skipping, no need to update {output_folder}.")
