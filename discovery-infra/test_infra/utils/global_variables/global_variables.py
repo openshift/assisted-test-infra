0dd21d8af776614cbd739070e9f554b6f54bc77f
Elior Erez <16567084+eliorerz@users.noreply.github.com>
2021-07-01 12:26:25 +0300

    NO-ISSUE - Fix SNO lack of ram and cpu (#931)

--- discovery-infra/test_infra/utils/global_variables/global_variables.py
+++ discovery-infra/test_infra/utils/global_variables/global_variables.py
@@ -1,8 +1,10 @@
+import logging
 from typing import ClassVar, List
 
 from frozendict import frozendict
 
 from test_infra import consts
+from test_infra.consts import resources
 from test_infra.utils.global_variables.env_variables_utils import _EnvVariablesUtils
 
 
@@ -12,10 +14,14 @@ _triggers = frozendict({
         "vip_dhcp_allocation": False
     },
     ("masters_count", 1): {
+        "workers_count": 0,
+        "nodes_count": 1,
         "high_availability_mode": consts.HighAvailabilityMode.NONE,
         "user_managed_networking": True,
         "vip_dhcp_allocation": False,
-        "openshift_version": consts.OpenshiftVersion.VERSION_4_8.value
+        "openshift_version": consts.OpenshiftVersion.VERSION_4_8.value,
+        "master_memory": resources.DEFAULT_MASTER_SNO_MEMORY,
+        "master_vcpu": resources.DEFAULT_MASTER_SNO_CPU
     },
     ("is_ipv6", True): {
         "service_network_cidr": consts.DEFAULT_IPV6_SERVICE_CIDR,
@@ -33,14 +39,11 @@ class GlobalVariables(_EnvVariablesUtils):
         super().__post_init__()
 
         for (env, expected), values in _triggers.items():
-            is_triggered = False
             if getattr(self, env) == expected:
                 for k, v in values.items():
                     self._set(k, v)
-                    is_triggered = True
-
-            if is_triggered:
                 self._triggered.append(env)
+                logging.info(f"{env.upper()} is triggered. Updating global variables: {values}")
 
     def __getattribute__(self, item):
         try:
