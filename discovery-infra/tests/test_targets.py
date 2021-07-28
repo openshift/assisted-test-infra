46418fc325a4c3b71fc3372ad49fda922ed654e1
Elior Erez <16567084+eliorerz@users.noreply.github.com>
2021-07-28 13:25:27 +0300

    NO-ISSUE - Rename target tests names (#999)

--- discovery-infra/tests/test_targets.py
+++ discovery-infra/tests/test_targets.py
@@ -6,9 +6,9 @@ from tests.base_test import BaseTest
 class TestMakefileTargets(BaseTest):
 
     @JunitTestSuite()
-    def test_deploy_nodes(self, cluster):
+    def test_target_deploy_nodes(self, cluster):
         cluster.prepare_for_installation()
 
     @JunitTestSuite()
-    def test_install_with_deploy_nodes(self, prepared_cluster):
+    def test_target_install_with_deploy_nodes(self, prepared_cluster):
         prepared_cluster.start_install_and_wait_for_installed()
