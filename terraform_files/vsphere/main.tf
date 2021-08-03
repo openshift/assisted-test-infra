a9f4156e1fb09bd7821afab407e1669b2384adf0
Michael Levy <milevy@redhat.com>
2021-08-03 15:59:41 +0300

    MGMT-7153: add vsphere worker nodes Support it in terraform and update vsphere controller Add NETWORK_TYPE to the skipper env file

--- terraform_files/vsphere/main.tf
+++ terraform_files/vsphere/main.tf
@@ -76,7 +76,7 @@ resource "vsphere_file" "ISO_UPLOAD" {
 }
 
 # Creating the master VMs.
-resource "vsphere_virtual_machine" "vm" {
+resource "vsphere_virtual_machine" "master" {
   count = var.masters_count
 
   name                        = "${var.cluster_name}-master-${count.index}"
@@ -99,6 +99,46 @@ resource "vsphere_virtual_machine" "vm" {
   disk {
     label            = "master-${count.index}-disk-0"
     size             = var.master_disk_size_gib
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+
+  dynamic "cdrom" {
+    # create the cdrom device only if the iso file is available
+    for_each = local.hasISO ? [1] : []
+    content {
+      datastore_id = data.vsphere_datastore.datastore.id
+      path         = local.hasISO ? vsphere_file.ISO_UPLOAD[0].destination_file : null
+    }
+  }
+
+  tags = [vsphere_tag.tag.id]
+}
+
+# Creating the workers VMs.
+resource "vsphere_virtual_machine" "worker" {
+  count = var.workers_count
+
+  name                        = "${var.cluster_name}-worker-${count.index}"
+  resource_pool_id            = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id                = data.vsphere_datastore.datastore.id
+  num_cpus                    = var.worker_vcpu
+  num_cores_per_socket        = var.vsphere_control_plane_cores_per_socket
+  memory                      = var.worker_memory
+  guest_id                    = "coreos64Guest"
+  folder                      = vsphere_folder.folder.path
+  enable_disk_uuid            = "true"
+  # no network before booting from the ISO file, which isn't available until prepare_for_installation stage
+  wait_for_guest_net_routable = local.hasISO
+  wait_for_guest_net_timeout  = local.hasISO ? 5 : 0
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label            = "worker-${count.index}-disk-0"
+    size             = var.worker_disk_size_gib
     eagerly_scrub    = false
     thin_provisioned = true
   }
