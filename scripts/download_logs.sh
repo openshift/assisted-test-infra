90106a50bab3215c1b6fc8e2111de71388132c01
Elior Erez <eerez@redhat.com>
2021-08-08 12:27:24 +0300

    Merge branch 'master' into NO-ISSUE-Add_junit_reports_for_download_logs.py

--- scripts/download_logs.sh
+++ scripts/download_logs.sh
@@ -48,7 +48,7 @@ function download_cluster_logs() {
     fi
   fi
 
-  skipper run ./discovery-infra/download_logs.py ${SERVICE_URL} ${LOGS_DEST} --cluster-id ${CLUSTER_ID} ${ADDITIONAL_PARAMS} --junit-report-dir ${JUNIT_REPORT_DIR}
+  skipper run -e JUNIT_REPORT_DIR ./discovery-infra/download_logs.py ${SERVICE_URL} ${LOGS_DEST} --cluster-id ${CLUSTER_ID} ${ADDITIONAL_PARAMS}
 }
 
 function collect_kube_api_resources() {
