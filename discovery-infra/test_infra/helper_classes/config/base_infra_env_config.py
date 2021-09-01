ee3f1bb0a2bd9cfb61a9d9ec533a094e1ea2c673
Elior Erez <16567084+eliorerz@users.noreply.github.com>
2021-09-01 14:16:40 +0300

    MGMT-7727 - Remove redundant cluster and entity config properties (#1074)

--- discovery-infra/test_infra/helper_classes/config/base_infra_env_config.py
+++ discovery-infra/test_infra/helper_classes/config/base_infra_env_config.py
@@ -5,29 +5,14 @@ from dataclasses import dataclass
 from .base_entity_config import BaseEntityConfig
 
 
-
 @dataclass
 class BaseInfraEnvConfig(BaseEntityConfig, ABC):
     """
     Define all configurations variables that are needed for Cluster during it's execution
     All arguments must have default to None with type hint
     """
-    pull_secret: str = None
-    ssh_public_key: str = None
-    openshift_version: str = None
     infra_env_id: str = None
     infra_env_name: str = None
-    additional_ntp_source: str = None
-    user_managed_networking: bool = None
-    high_availability_mode: str = None
-    hyperthreading: str = None
-    iso_download_path: str = None  # Todo - Might change during MGMT-5370
-    iso_image_type: str = None
-    nodes_count: int = None  # Todo - Might change during MGMT-5370
-    download_image: bool = None
-    platform: str = None
-    is_static_ip: bool = None
-    is_ipv6: bool = None  # Todo - Might change during MGMT-5370
 
     def is_cluster(self) -> bool:
         return False
--- discovery-infra/test_infra/helper_classes/config/cluster_config.py
+++ discovery-infra/test_infra/helper_classes/config/cluster_config.py
@@ -14,23 +14,11 @@ class BaseClusterConfig(BaseEntityConfig, ABC):
     Define all configurations variables that are needed for Cluster during it's execution
     All arguments must have default to None with type hint
     """
-    pull_secret: str = None
-    ssh_public_key: str = None
-    openshift_version: str = None
     cluster_id: str = None
     cluster_name: ClusterName = None
-    additional_ntp_source: str = None
-    user_managed_networking: bool = None
-    high_availability_mode: str = None
-    hyperthreading: str = None
     olm_operators: List[str] = None
     base_dns_domain: str = None
     vip_dhcp_allocation: bool = None
-    iso_download_path: str = None
-    iso_image_type: str = None
-    download_image: bool = None
-    platform: str = None
-    is_static_ip: bool = None
     service_network_cidr: str = None
     cluster_network_cidr: str = None
     cluster_network_host_prefix: int = None
