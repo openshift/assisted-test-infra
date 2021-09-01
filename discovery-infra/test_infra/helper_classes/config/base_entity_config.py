30cf37a25465fab5f63683b87aab1786dd2e1241
Elior Erez <eerez@redhat.com>
2021-09-01 12:39:39 +0300

    MGMT-7727 - Remove redundant cluster and entity config properties

--- discovery-infra/test_infra/helper_classes/config/base_entity_config.py
+++ discovery-infra/test_infra/helper_classes/config/base_entity_config.py
@@ -7,6 +7,19 @@ from .base_config import _BaseConfig
 
 @dataclass
 class BaseEntityConfig(_BaseConfig, ABC):
+    pull_secret: str = None
+    ssh_public_key: str = None
+    openshift_version: str = None
+    additional_ntp_source: str = None
+    user_managed_networking: bool = None
+    high_availability_mode: str = None
+    hyperthreading: str = None
+    iso_download_path: str = None
+    iso_image_type: str = None
+    download_image: bool = None
+    platform: str = None
+    is_static_ip: bool = None
+    is_ipv6: bool = None
 
     @abstractmethod
     def is_cluster(self) -> bool:
