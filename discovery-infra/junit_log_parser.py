2a207511d81a2e8487869162a3fc2f8fbf8b47a3
Elior Erez <16567084+eliorerz@users.noreply.github.com>
2021-11-01 13:44:03 +0200

    NO-ISSUE - Fix junit report log parser adding multiple success cases (#1202)

--- discovery-infra/junit_log_parser.py
+++ discovery-infra/junit_log_parser.py
@@ -4,7 +4,7 @@ import uuid
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import List, Dict, Optional
 
 from junit_xml import TestCase, TestSuite, to_xml_report_string
 
@@ -28,20 +28,49 @@ class LogEntry:
     msg: str = None
     func: str = None
     file: str = None
+    error: Optional[str] = None
 
 
 LOG_FORMAT = r'time="(?P<time>(.*?))" ' \
              r'level=(?P<level>(.*?)) ' \
              r'msg="(?P<msg>.*)" ' \
              r'func=(?P<func>.*) ' \
-             r'file="(?P<file>(.*?))"'
+             r'file="(?P<file>(.*?))" ' \
+             r'(error="(?P<error>.*)")?'
+
 
 EXPORTED_LOG_LEVELS = ("fatal", "error")
 EXPORTED_EVENT_LEVELS = ("critical", "error")
 
 
+def is_duplicate_entry(entry: LogEntry, entry_message: str, fail_cases: Dict[str, List[TestCase]]) -> bool:
+    for case in fail_cases.get(entry.func, []):
+        if case.failures and case.failures[0].message == entry_message:
+            return True
+    return False
+
+
+def get_log_entry_case(entry: LogEntry, fail_cases: Dict[str, List[TestCase]], suite_name: str) -> List[TestCase]:
+    fail_case: List[TestCase] = list()
+    message = f"{entry.msg}\n{entry.error if entry.error else ''}"
+
+    if is_duplicate_entry(entry, message, fail_cases):
+        return []
+
+    test_case = TestCase(name=entry.func, classname=suite_name, category=suite_name, timestamp=entry.time)
+    test_case.failures.append(CaseFailure(message=message, output=message, type=entry.level))
+    fail_case.append(test_case)
+
+    if entry.level != "fatal":
+        # Add test case with the same name so it will be marked in PROW as flaky
+        flaky_test_case = TestCase(name=entry.func, classname=suite_name, category=suite_name)
+        fail_case.append(flaky_test_case)
+
+    return fail_case
+
+
 def get_failure_cases(log_file_name: Path, suite_name: str) -> List[TestCase]:
-    fail_cases = list()
+    fail_cases: Dict[str, List[TestCase]] = dict()
 
     with open(log_file_name) as f:
         for line in f:
@@ -52,25 +81,18 @@ def get_failure_cases(log_file_name: Path, suite_name: str) -> List[TestCase]:
             entry = LogEntry(**values.groupdict())
             if entry.level not in EXPORTED_LOG_LEVELS:
                 continue
-
-            test_case = TestCase(name=entry.func, classname=suite_name, category=log_file_name, timestamp=entry.time)
-            test_case.failures.append(CaseFailure(message=entry.msg, output=entry.msg, type=entry.level))
-            fail_cases.append(test_case)
-
-            # test is flaky
-            if entry.level != "fatal":
-                # Add test case with the same name so it will be marked in PROW as flaky
-                flaky_test_case = TestCase(name=entry.func, classname=suite_name, category=log_file_name)
-                fail_cases.append(flaky_test_case)
+            if entry.func not in fail_cases:
+                fail_cases[entry.func] = list()
+            fail_cases[entry.func] += get_log_entry_case(entry, fail_cases, suite_name)
 
     log.info(f"Found {len(fail_cases)} failures on {suite_name} suite")
-    return fail_cases
+    return [c for cases in fail_cases.values() for c in cases]
 
 
 def export_service_logs_to_junit_suites(source_dir: Path, report_dir: Path):
     suites = list()
     for file in source_dir.glob("k8s_assisted-service*.log"):
-        suite_name = Path(file).stem
+        suite_name = Path(file).stem.replace("k8s_", "")
         log.info(f"Creating test suite from {suite_name}.log")
         test_cases = get_failure_cases(file, suite_name)
         timestamp = test_cases[0].timestamp if test_cases else None
