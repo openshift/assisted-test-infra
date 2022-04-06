1c71794a882b76967a7deb7aa974f736db9314fc
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2022-04-06 04:30:54 -0700

    MGMT-9852 - Fix image-pulling script. Fix missing images when using multi-stage builds. Sleep between retries. Ignore images aliases using regex. (#1597)
    Co-authored-by: Elior Erez <eerez@redhat.com>

--- scripts/pull_dockerfile_images.sh
+++ scripts/pull_dockerfile_images.sh
@@ -2,21 +2,28 @@
 
 export CONTAINER_COMMAND=${CONTAINER_COMMAND:-podman}
 
-images=$(cat Dockerfile.* ./*/Dockerfile.* | grep FROM | awk '{print $2}')
-echo "### Attempting to download assisted dockerfile images (best effort) ###"
+images=$(cat Dockerfile.* ./*/Dockerfile.* | grep -i FROM | awk '{print $2}')
+images=${images//--from=}
+
+echo "### Attempting to pull assisted dockerfile images (best effort) ###"
 echo "Images found: ${images}"
 
 for image in ${images}; do
-  if [[ ${image} =~ (.*:.*) ]]; then
+  if [[ ${image} =~ (.*\/.*:.*) ]]; then
       for i in {1..5}; do
-        echo "Image ${image} ${i} download attempt ${image}"
-        podman pull "${image}" || rc=$?
-
+        echo "Image ${image} - pull attempt ${i}"
+        podman pull "${image}" ; rc=$?
         if [[ "${rc}" -eq 0 ]]; then
+          echo "Image pulled successfully"
           break
         fi
 
-        echo "Failed to download image ${image}, retrying ..."
+        echo "Failed to pull image ${image}"
+        if [[ "${i}" -ne 5 ]]; then
+          echo "Retrying ..."
+          sleep 5
+        fi
+
       done
   fi
 done
