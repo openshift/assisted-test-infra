4a7c84ebf7a4551fae27780f208926c1b1c005b2
Elior Erez <eerez@redhat.com>
2022-04-10 15:38:03 +0300

    MGMT-9555 - Add more complex triggers conditions comparison methods

--- src/triggers/env_trigger.py
+++ src/triggers/env_trigger.py
@@ -1,5 +1,7 @@
+import inspect
+import re
 from abc import ABC, abstractmethod
-from typing import Any, Dict, List, Tuple, Union
+from typing import Any, Callable, Dict, List
 
 from assisted_test_infra.test_infra.utils import EnvVar
 from service_client import log
@@ -27,13 +29,13 @@ class Triggerable(ABC):
         except AttributeError:
             return False
 
-    def handle_trigger(self, cond: Union[Tuple[str, Any], Tuple[Tuple[str, Any]]], values: Dict[str, Any]) -> None:
+    def handle_trigger(self, conditions_string: List[str], values: Dict[str, Any]) -> None:
         for k, v in values.items():
             if not hasattr(self, k):
                 continue
 
             if not self.is_user_set(k):
-                log.debug(f"{self.__class__.__name__} - Trigger set {k} to {v}, Condition: {cond}")
+                log.debug(f"{self.__class__.__name__} - Trigger set `{k}` to `{v}`, Condition: {conditions_string}")
                 self._set(k, v)
             else:
                 log.warning(f"Skipping setting {k} to value {v} due that it already been set by the user")
@@ -46,22 +48,19 @@ class Triggerable(ABC):
 class Trigger:
     """Mechanism for applying pre-known configurations if a given trigger condition was met"""
 
-    def __init__(self, condition: Union[Tuple[str, Any], Tuple[Tuple[str, Any], Tuple[str, Any]]], **kwargs):
-        self._conditions = condition
+    def __init__(self, *, condition: Callable[[Triggerable], bool], **kwargs):
+        self._condition = condition
         self._variables_to_set = kwargs
+        self._conditions_string = re.findall(r"(lambda.*),", str(inspect.getsourcelines(condition)[0]))
 
     def is_condition_met(self, config: Triggerable):
-        conditions = self._conditions
-        if not isinstance(conditions[0], tuple):
-            conditions = (conditions,)
-        return all(self._is_set(config, param, expected_value) for param, expected_value in conditions)
+        try:
+            return self._condition(config)
+        except AttributeError:
+            return False
 
-    def handle_trigger(self, config: Triggerable):
-        config.handle_trigger(self._conditions, self._variables_to_set)
-
-    @classmethod
-    def _is_set(cls, config, var, expected_value):
-        return getattr(config, var, None) == expected_value
+    def handle(self, config: Triggerable):
+        config.handle_trigger(self._conditions_string, self._variables_to_set)
 
     @classmethod
     def trigger_configurations(cls, configs: List[Triggerable], default_triggers: dict):
@@ -76,4 +75,4 @@ class Trigger:
         for trigger_name, trigger in met_triggers.items():
             for config in configs:
                 log.info(f"Handling {trigger_name} trigger")
-                trigger.handle_trigger(config)
+                trigger.handle(config)
--- src/triggers/olm_operators_trigger.py
+++ src/triggers/olm_operators_trigger.py
@@ -1,4 +1,5 @@
 from contextlib import suppress
+from typing import Callable
 
 from assisted_test_infra.test_infra.utils.operators_utils import resource_param
 from consts import OperatorResource
@@ -6,28 +7,19 @@ from triggers.env_trigger import Trigger, Triggerable
 
 
 class OlmOperatorsTrigger(Trigger):
-    def __init__(self, condition: str, **kwargs):
-        super().__init__(condition, **kwargs)
+    def __init__(self, condition: Callable[[Triggerable], bool], operator: str):
+        super().__init__(condition=condition, operator=operator)
+        self._operator = operator
 
-    def is_condition_met(self, config: Triggerable):
-        if hasattr(config, "olm_operators") and self._conditions in getattr(config, "olm_operators", []):
-            return True
-
-        return False
-
-    def handle_trigger(self, config: Triggerable):
+    def handle(self, config: Triggerable):
         variables_to_set = self.get_olm_variables(config)
-        config.handle_trigger(self._conditions, variables_to_set)
+        config.handle_trigger(self._conditions_string, variables_to_set)
 
     def get_olm_variables(self, config: Triggerable) -> dict:
         operator_variables = {}
-        operator = self._conditions
-
-        if not isinstance(operator, str):
-            return {}
 
         for key in OperatorResource.get_resource_dict().keys():
             with suppress(AttributeError):
-                operator_variables[key] = resource_param(getattr(config, key), key, operator)
+                operator_variables[key] = resource_param(getattr(config, key), key, self._operator)
 
         return operator_variables
