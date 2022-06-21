cec1e21316f2a7bedb6d8a99ac2f35ae7dff4eb2
Elior Erez <eerez@redhat.com>
2022-06-21 12:01:47 +0300

    NO-ISSUE - Use the default node controller in day2 test

--- src/tests/test_day2.py
+++ src/tests/test_day2.py
@@ -9,11 +9,11 @@ class TestDay2(BaseTest):
     # Install day1 cluster and deploy day2 nodes (cloud flow).
     # Or, deploy day2 nodes on an installed cluster if CLUSTER_ID env var is specified.
     @JunitTestSuite()
-    def test_deploy_day2_nodes_cloud(self, cluster, day2_cluster):
+    def test_deploy_day2_nodes_cloud(self, cluster, day2_cluster, controller):
         if not global_variables.cluster_id:
             cluster.prepare_for_installation()
             cluster.start_install_and_wait_for_installed()
 
         day2_cluster.config.day1_cluster_id = cluster.id
         day2_cluster.prepare_for_installation()
-        day2_cluster.start_install_and_wait_for_installed()
+        day2_cluster.start_install_and_wait_for_installed(controller)
