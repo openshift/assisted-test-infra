53533a3e6dd8adbcc06e46c3763095784d947fb0
Elior Erez <eerez@redhat.com>
2022-09-09 23:13:00 +0300

    NO-ISSUE - Upgrade to python 3.10

--- src/update_assisted_service_cm.py
+++ src/update_assisted_service_cm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Idea is to pass os environments to assisted-service config map, to make an easy way to configure assisted-service
