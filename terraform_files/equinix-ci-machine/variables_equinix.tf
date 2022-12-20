536ca778164426cfdcf3e417bb16b48dfa469597
Adrien Gentil <agentil@redhat.com>
2022-12-20 16:15:29 +0100

    MGMT-12439: Deploy heterogeneous infrastructure (#1949)

---
+++ terraform_files/equinix-ci-machine/variables_equinix.tf
@@ -0,0 +1,15 @@
+variable "devices" {
+  description = "Settings for desired devices"
+  type = list(
+    object({
+      facilities           = optional(list(string), ["any"])
+      hostname             = string
+      operating_system     = optional(string, "rocky_8")
+      plan                 = string
+      project_id           = string
+      ssh_private_key_path = string
+      ssh_user             = optional(string, "root")
+      tags                 = optional(list(string), [])
+    })
+  )
+}
