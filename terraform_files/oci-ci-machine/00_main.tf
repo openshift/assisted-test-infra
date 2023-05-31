67ddc7a09b9ed8ee0f3f7b4a4902d096eebbfd4a
Adrien Gentil <agentil@redhat.com>
2023-05-31 15:25:22 +0200

    set credentials

--- terraform_files/oci-ci-machine/00_main.tf
+++ terraform_files/oci-ci-machine/00_main.tf
@@ -8,4 +8,9 @@ terraform {
 }
 
 provider "oci" {
+  tenancy_ocid     = var.oci_tenancy_id
+  user_ocid        = var.oci_user_id
+  fingerprint      = var.oci_fingerprint
+  private_key_path = var.oci_private_key_path
+  region           = var.oci_region
 }
--- terraform_files/oci-ci-machine/variables.tf
+++ terraform_files/oci-ci-machine/variables.tf
@@ -3,6 +3,16 @@ variable "unique_id" {
   description = "Identifier used to tag all and suffix all the ressource names related to the current job"
 }
 
+variable "private_ssh_key_path" {
+  type        = string
+  description = "Path to private key"
+}
+
+variable "public_ssh_key_path" {
+  type        = string
+  description = "Path to public key"
+}
+
 ///////////
 // OCI variables
 ///////////
@@ -12,37 +22,27 @@ variable "oci_compartment_id" {
   description = "Parent compartment where the resources will be created"
 }
 
-variable "tenancy_oicd" {
+variable "oci_tenancy_id" {
   type        = string
-  description = ""
+  description = "tenancy OCID authentication value"
 }
 
-variable "user_oicd" {
+variable "oci_user_id" {
   type        = string
-  description = ""
+  description = "user OCID authentication value"
 }
 
-variable "fingerprint" {
+variable "oci_fingerprint" {
   type        = string
-  description = ""
+  description = "key fingerprint authentication value"
 }
 
-variable "private_key_path" {
+variable "oci_private_key_path" {
   type        = string
-  description = ""
+  description = "private key path authentication value"
 }
 
-variable "region" {
+variable "oci_region" {
   type        = string
-  description = ""
-}
-
-variable "private_ssh_key_path" {
-  type        = string
-  description = "Path to private key"
-}
-
-variable "public_ssh_key_path" {
-  type        = string
-  description = "Path to public key"
+  description = "OCI region"
 }
