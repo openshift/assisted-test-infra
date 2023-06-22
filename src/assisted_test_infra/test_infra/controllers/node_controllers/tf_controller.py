873f9f6dddd7b8de2604c4e5cae87c74f6321a26
Adrien Gentil <agentil@redhat.com>
2023-06-22 14:04:11 +0200

    remove abstractmethod on set_dns

--- src/assisted_test_infra/test_infra/controllers/node_controllers/tf_controller.py
+++ src/assisted_test_infra/test_infra/controllers/node_controllers/tf_controller.py
@@ -101,7 +101,6 @@ class TFController(NodeController, ABC):
         for node in nodes:
             self.shutdown_node(node.name)
 
-    @abstractmethod
     def set_dns(self, api_ip: str, ingress_ip: str) -> None:
         utils.add_dns_record(
             cluster_name=self.cluster_name,
