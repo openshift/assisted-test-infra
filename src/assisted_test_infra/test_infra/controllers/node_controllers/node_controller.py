3b519744eb7c890874060b2ca4ed7712ab74efe0
Elior Erez <eerez@redhat.com>
2023-08-21 15:43:18 +0300

    MGMT-15510: Initialize terraform controller after cluster registration

--- src/assisted_test_infra/test_infra/controllers/node_controllers/node_controller.py
+++ src/assisted_test_infra/test_infra/controllers/node_controllers/node_controller.py
@@ -40,6 +40,9 @@ class NodeController(ABC):
     def tf_platform(self):
         return self._config.tf_platform
 
+    def init_controller(self):
+        pass
+
     @abstractmethod
     def list_nodes(self) -> List[Node]:
         pass
