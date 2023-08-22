c48fbc1d8d43e857872f1761774f4ac5dc6ec42b
Adrien Gentil <agentil@redhat.com>
2023-08-22 15:16:57 +0200

    NO-ISSUE: Use a regex to select OS image in OCI
    OS images are updated regulary and old ones expire. In order to avoid
    spending time to update OS images when job start to fail, use a regex to
    select the right OS image.

--- terraform_files/oci-ci-machine/00_main.tf
+++ terraform_files/oci-ci-machine/00_main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "5.0.0"
+      version = "5.9.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
