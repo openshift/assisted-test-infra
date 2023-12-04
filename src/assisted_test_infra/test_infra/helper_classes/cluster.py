7a91650fe63e314a3e6e8a14bf8ec209fdda7431
Adrien Gentil <agentil@redhat.com>
2023-12-04 16:09:33 +0100

    add the ability to update installer args

--- src/assisted_test_infra/test_infra/helper_classes/cluster.py
+++ src/assisted_test_infra/test_infra/helper_classes/cluster.py
@@ -884,6 +884,8 @@ class Cluster(BaseCluster):
         if self._high_availability_mode != consts.HighAvailabilityMode.NONE:
             self.set_host_roles(len(self.nodes.get_masters()), len(self.nodes.get_workers()))
 
+        self._infra_env.update_host_installer_args()
+
     @JunitTestCase()
     def create_custom_manifests(self):
         log.info(f"Adding {len(self._config.custom_manifests)} custom manifests")
