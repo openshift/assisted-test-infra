402e716c11b9fec4feeb6bfb255f1949309d459f
Riccardo Piccoli <rpiccoli@redhat.com>
2024-01-10 09:55:31 +0100

    MGMT-16330: disallow SDN network type testing for 4.15 and above (#2358)
    Signed-off-by: Riccardo Piccoli <rpiccoli@redhat.com>

--- src/tests/test_e2e_install.py
+++ src/tests/test_e2e_install.py
@@ -1,8 +1,10 @@
 import pytest
+import semver
 from junit_report import JunitTestSuite
 
 import consts
 from tests.base_test import BaseTest
+from tests.config import global_variables
 from tests.conftest import get_available_openshift_versions, get_supported_operators
 
 
@@ -21,6 +23,11 @@ class TestInstall(BaseTest):
     @JunitTestSuite()
     @pytest.mark.parametrize("network_type", [consts.NetworkType.OpenShiftSDN, consts.NetworkType.OVNKubernetes])
     def test_networking(self, cluster, network_type):
+        if semver.Compare(global_variables.openshift_version, "4.15.0") >= 0:
+            raise ValueError(
+                "parametrization of network type not necessary from 4.15.0 and above,"
+                " as the only supported network type is OVN"
+            )
         cluster.prepare_for_installation()
         cluster.start_install_and_wait_for_installed(fall_on_pending_status=True)
 
