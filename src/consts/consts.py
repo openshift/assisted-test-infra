f68a5ff2b267997a9dd5607c4f2d27fe1b556ce1
Dan Manor <dmanor@redhat.com>
2024-09-22 15:53:56 +0300

    MGMT-18959: Enable OCP 4.18 installation (#2535)

--- src/consts/consts.py
+++ src/consts/consts.py
@@ -16,6 +16,7 @@ class OpenshiftVersion(Enum):
     VERSION_4_15 = "4.15"
     VERSION_4_16 = "4.16"
     VERSION_4_17 = "4.17"
+    VERSION_4_18 = "4.18"
     DEFAULT = VERSION_4_16
     MULTI_VERSION = "all"
 
