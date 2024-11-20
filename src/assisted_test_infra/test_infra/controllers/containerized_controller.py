0b784b3cf44b40659af98843f3e0df9c945bb99f
Benny Kopilov <bkopilov@redhat.com>
2024-11-20 11:15:49 +0200

    Allow to modify files inside running container. (#2558)
    The code will allow to change containers files when its up
    boot from iscsi volume requires different pxe setting per node.
    Instead of creating multiple pxe, re-using existing pxe by changing
    the pxe disk volume.

--- src/assisted_test_infra/test_infra/controllers/containerized_controller.py
+++ src/assisted_test_infra/test_infra/controllers/containerized_controller.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC
 from typing import List, Optional
 
@@ -12,6 +13,7 @@ class ContainerizedController(ABC):
         self._image = image
         self._extra_flags = [] if not extra_flags else extra_flags
         self._is_running = False
+        self._container_id = None
 
     def run(self, **kwargs):
         if self._is_running:
@@ -29,6 +31,43 @@ class ContainerizedController(ABC):
         ]
 
         utils.run_container(container_name=self._name, image=self._image, flags=base_run_flags + self._extra_flags)
+
+    @property
+    def container_id(self):
+        # Cached container id when created.
+        if not self._is_running:
+            raise RuntimeError("Unable to get container id when container is not running ")
+
+        if self._container_id is None:
+            podman_filter_name = f"podman-remote ps -f name={self._name} --format json"
+            out = utils.run_command(podman_filter_name, shell=True)[0]
+            podman_id = json.loads(out)[0]["Id"]
+            self._container_id = podman_id
+
+        return self._container_id
+
+    def write_to_container(self, file_path_container: str, content: list[str], append: bool = False) -> None:
+        # Write to file inside the container - update running container
+        container_id = self.container_id
+
+        redirect_append = ">>"
+        redirect_override = ">"
+        empty = "\n"
+        exec_container = f"podman-remote exec -it {container_id} /bin/sh -c"
+
+        if not append:
+            # Reset file content if not append
+            utils.run_command(f"{exec_container} 'echo {empty}{redirect_override}{file_path_container}'", shell=True)
+
+        for line in content:
+            utils.run_command(f"{exec_container} 'echo {line}{redirect_append}{file_path_container}'", shell=True)
+
+    def copy_file_to_container(self, local_file_path: str, container_file_path: str) -> None:
+        # Copy file from local hypervisor to the running container
+        container_id = self.container_id
+
+        exec_container = f"podman-remote cp {local_file_path}  {container_id}:{container_file_path}"
+        utils.run_command(f"{exec_container}", shell=True)
 
     def remove(self):
         if self._is_running:
