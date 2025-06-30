ccb3ada117309234172f913498091dde440eb098
Dan Manor <dmanor@redhat.com>
2025-06-30 18:59:04 +0300

    MGMT-20671: Add AWS heterogeneous playbooks (#2698)

--- terraform_files/equinix-ci-machine/output.tf
+++
@@ -1,9 +0,0 @@
-output "inventory" {
-  value = [for d in equinix_metal_device.ci_devices : {
-    "access_private_ipv4" : d.access_private_ipv4,
-    "access_public_ipv4" : d.access_public_ipv4
-    "hostname" : d.hostname,
-    "tags" : d.tags,
-    }
-  ]
-}
